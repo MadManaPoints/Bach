7b87fe2bf6f8ecbd47d20832bdfdaefa172f273c
Manny Patrick Vivas <mannyvivas21@gmail.com>
2025-07-04 15:37:19 -0400

    updated slider

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -4,22 +4,24 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-   [SerializeField] private Slider  _thrusterSlider;
-   [SerializeField] private Slider _sensitivitySlider;
+    [SerializeField] Slider _thrusterSlider;
+    [SerializeField] Slider _sensitivitySlider;
     PlayerMovement player;
     public float initValue, sensitivityBar;
 
-    public void UpdateInputBar (float initValue){
-        if (initValue < 0) {
+    public void UpdateInputBar(float initValue)
+    {
+        if (initValue < 0)
+        {
             Debug.Log("No Voice Input");
             _thrusterSlider.value = 0;
-        } else 
+        }
+        else
 
-        _thrusterSlider.value = initValue;
+            _thrusterSlider.value = initValue;
 
     }
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         player = PlayerMovement.player;
@@ -31,26 +33,40 @@ public class UI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (initValue>1){
-            
-            _thrusterSlider.value = initValue/100.0f;
+        /*if (initValue > 1)
+        {
+
+            _thrusterSlider.value = initValue / 100.0f;
         }
 
         if (Input.GetKeyDown("space"))
         {
-            _thrusterSlider.value +=10;
+            _thrusterSlider.value += 10;
             Debug.Log("Space Pressed");
         }
 
-        if (_sensitivitySlider.value>=sensitivityBar){
-            Debug.Log (sensitivityBar);
+        if (_sensitivitySlider.value >= sensitivityBar)
+        {
+            Debug.Log(sensitivityBar);
             _sensitivitySlider.value = sensitivityBar;
         }
-        if (_sensitivitySlider.value<sensitivityBar){
-            sensitivityBar=_sensitivitySlider.value;
+        if (_sensitivitySlider.value < sensitivityBar)
+        {
+            sensitivityBar = _sensitivitySlider.value;
 
         }
-            _sensitivitySlider.value = sensitivityBar;
+        _sensitivitySlider.value = sensitivityBar;*/
+
+
+        _thrusterSlider.value = map(player.loudness, 0, 3, 0, 100);
+    }
+    
+    float map(float value, float minA, float maxA, float minB, float maxB){
+        float range = maxA - minA; 
+        float valuePercent = (value - minA) / range;
+
+        float newRange = maxB - minB;
         
+        return valuePercent * newRange + minB;
     }
 }
